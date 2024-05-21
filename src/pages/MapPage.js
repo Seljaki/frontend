4ef5d270eda250a14ced78d32dcecc72fad93b60
MrDog210 <tusakf@gmail.com>
2024-05-21 10:19:47 +0200

    SCRUM-70 updated max bounds of map

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1,70 +1,7 @@
-import { MapContainer, WMSTileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import "proj4leaflet";
-import { Proj} from 'leaflet';
-import { Box, useTheme } from '@mui/material';
+import GerkMap from "../components/map/GerkMap"
 
 function MapPage() {
-  const theme = useTheme();
-
-  const sloCRS = new Proj.CRS("EPSG:3794","+proj=tmerc +lat_0=0 +lon_0=15 +k=0.9999 +x_0=500000 +y_0=-5000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +type=crs",
-  {
-    resolutions: [
-      262144, 131072, 65536, 32768, 16384, 8192, 4096, 2048, 1024, 512, 256,
-      128, 64, 28, 16, 8, 4, 2, 1
-    ],
-  });
-
-  return (
-    <Box style={{ minWidth: "100%", height: "100vh"}}>
-      
-      <MapContainer style={{ height: "100%", width: "100%", backgroundColor: theme.palette.background.default}}  maxZoom={18} center={[46.413874, 16.063868]} crs={sloCRS} zoom={18} scrollWheelZoom={true}>
-      <WMSTileLayer transparent={true}
-          layers='OI.OrthoimageCoverage' //'SI.GURS.ZPDZ%3ADOF025'
-          url='https://storitve.eprostor.gov.si/ows-ins-wms/oi/ows/ows?'
-          params={{
-            //transparent: true,
-            format: 'image/png'
-          }}
-        />
-        <WMSTileLayer transparent={true}
-          layers='SI.GURS.RPE:OBCINE' //'SI.GURS.ZPDZ%3ADOF025'
-          url='https://ipi.eprostor.gov.si/wms-si-gurs-rpe/wms?'
-          params={{
-            transparent: true,
-            format: 'image/png'
-          }}
-        />
-        
-        {/* <WMSTileLayer transparent={true}
-          layers='SI.GURS.KN:PARCELE' //'SI.GURS.ZPDZ%3ADOF025'
-          url='https://ipi.eprostor.gov.si/wms-si-gurs-kn/wms?'
-          params={{
-            transparent: true,
-            format: 'image/png'
-          }}
-        /> */}
-
-        <WMSTileLayer transparent={true}
-          layers='nspire_common:DEFAULT' //'SI.GURS.ZPDZ%3ADOF025'
-          url='https://storitve.eprostor.gov.si/ows-ins-wms/gn/ows?'
-          params={{
-            transparent: true,
-            format: 'image/png'
-          }}
-        />
-
-        <WMSTileLayer transparent={true}
-          layers='GERK' //'SI.GURS.ZPDZ%3ADOF025'
-          url='https://rkg.gov.si/GERK/wms?'
-          params={{
-            transparent: true,
-            format: 'image/png'
-          }}
-        />
-      </MapContainer>
-    </Box>
-  )
+  return <GerkMap />
 }
 
 export default MapPage
