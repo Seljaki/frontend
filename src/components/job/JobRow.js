bd017f3a512b4d39e00234001c8ac7d3501dbbc9
MrDog210 <tusakf@gmail.com>
2024-05-28 22:03:36 +0200

    SCRUM-78 added job costs

--- src/components/job/JobRow.js
+++ src/components/job/JobRow.js
@@ -1,11 +1,84 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, IconButton, Typography } from "@mui/material"
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, IconButton, Typography } from "@mui/material"
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { getUnitForQuantityType } from "../../constants/jobs";
+import { useContext, useEffect, useState } from "react";
+import EditJobCost from "../jobCost/EditJobCost";
+import { UserContext } from "../../store/userContext";
+import { SERVER_URL } from "../../constants/http";
+import JobCostRow from "../jobCost/JobCostRow";
 
 function JobRow({job, onEdit = () => {}, onDelete = () => {}}) {
-  const { id, quantity, price, totalPrice, timeTaken, invoice_id, jobtype_id, jobType } = job
+  const { id, quantity, price, totalPrice, timeTaken, invoice_id, jobtype_id, jobType, totalCost } = job
+  const [ editingJobCost, setEditingJobCost ] = useState(null)
+  const [ jobCosts, setJobCosts ] = useState([])
+  const userCtx = useContext(UserContext)
+
+  useEffect(() => {
+    async function fetchAllJobCosts() {
+      const data = await fetch(SERVER_URL + `/jobs/${id}/jobCosts`, {
+        headers: {
+          "x-auth-token": userCtx.token,
+        },
+      });
+      if (data.status < 300) {
+        const json = await data.json();
+        setJobCosts(json.jobCosts)
+      }
+    }
+    fetchAllJobCosts()
+  }, [])
+
+  async function submitJobCost(jobCost) {
+    const data = await fetch(SERVER_URL + `/jobs/${id}/jobCosts`, {
+      method: 'POST',
+      headers: {
+        "x-auth-token": userCtx.token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jobCost)
+    });
+    if (data.status < 300) {
+      const json = await data.json();
+      setJobCosts([json.jobCost,...jobCosts])
+      setEditingJobCost(null)
+    }
+  }
+
+  async function updateJobCost(jobCost) {
+    const data = await fetch(SERVER_URL + `/jobCosts/${jobCost.id}`, {
+      method: 'PUT',
+      headers: {
+        "x-auth-token": userCtx.token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jobCost)
+    });
+    if (data.status < 300) {
+      const json = await data.json();
+      setJobCosts(jobCosts.map(jc => jc.id === jobCost.id ? json.jobCost : jc))
+      setEditingJobCost(null)
+    }
+  }
+
+  async function deleteJobCost(jobCostId) {
+    const data = await fetch(SERVER_URL + `/jobCosts/${jobCostId}`, {
+      method: 'DELETE',
+      headers: {
+        "x-auth-token": userCtx.token,
+      },
+    });
+    if (data.status < 300) {
+      const json = await data.json();
+      setJobCosts(jobCosts.filter(jc => jc.id !== jobCostId))
+    }
+  }
+
+  function onAddJobCostPressed() {
+    setEditingJobCost({ title: '', amount: 0 })
+  }
+
   return (
     <Accordion>
       <AccordionSummary
@@ -16,6 +89,7 @@ function JobRow({job, onEdit = () => {}, onDelete = () => {}}) {
           <span>količina: {quantity}</span>
           <span>cena na {getUnitForQuantityType(jobType.quantityType)}: {price} EUR</span>
           <span>končna cena: {totalPrice} EUR</span>
+          <span>končni stroški: {totalCost} EUR</span>
         </span>
         <span>
           <IconButton onClick={onEdit} color="primary"><EditIcon/></IconButton>
@@ -23,10 +97,20 @@ function JobRow({job, onEdit = () => {}, onDelete = () => {}}) {
         </span>
       </AccordionSummary>
       <AccordionDetails>
+      <Button onClick={onAddJobCostPressed}>Add job cost</Button>
+        { editingJobCost && <EditJobCost jobCost={editingJobCost} setJobCost={setEditingJobCost} onConfirm={() => {
+          if(editingJobCost.id)
+            updateJobCost(editingJobCost)
+          else
+            submitJobCost(editingJobCost)
+        }} />}
         <Typography>Zavzet čas: {timeTaken} min</Typography>
+        {jobCosts.map(jc => <JobCostRow key={jc.id} jobCost={jc}
+          onEdit={() => {setEditingJobCost(jc)}}
+          onDelete={() => {deleteJobCost(jc.id)}} 
+          />) }
         <Typography>TODO: PLOTS</Typography>
         <Typography>TODO: EQUIPMENT</Typography>
-        <Typography>TODO: cost</Typography>
       </AccordionDetails>
     </Accordion>
   )
