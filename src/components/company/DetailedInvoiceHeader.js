21935a1ff12d69b928a70637a063c9c4b5c15448
mzHap <mojcazamuda12@gmail.com>
2024-05-28 22:29:27 +0200

    fixed wordwrap and some margin post price

--- src/components/company/DetailedInvoiceHeader.js
+++ src/components/company/DetailedInvoiceHeader.js
@@ -16,16 +16,16 @@ function DetailedInvoiceHeader({ invoice }) {
       width:'100%'
     }}>
       <Paper sx={{ display: 'flex', flexDirection: 'column', p: 2, alignItems: 'center', width: '100%' }}>
-        <Typography variant="h4" sx={{ mb: 2, color: myTheme.palette.primary.main }}>{title}</Typography>
+        <Typography variant="h4" sx={{ mb: 2, color: myTheme.palette.primary.main, wordWrap:'break-word' }}>{title}</Typography>
         <Grid container sx={{ width: '100%', p:1 }}>
           <Grid item xs={10}>
-            <Typography variant="h6" sx={{ color: myTheme.palette.primary.main }}>Invoice note:</Typography>
-            <Typography>
+            <Typography sx={{wordWrap: 'break-word'}} variant="h6" sx={{ color: myTheme.palette.primary.main }}>Invoice note:</Typography>
+            <Typography sx={{wordWrap: 'break-word'}}>
               {note}
             </Typography>
           </Grid>
-          <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-            <Typography variant="h6">
+          <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', mb:1}}>
+            <Typography sx={{wordWrap: 'break-word'}} variant="h6">
               <span style={{ color: myTheme.palette.primary.main }}>TOTAL: </span>{totalPrice} €
             </Typography>
           </Grid>
--- src/components/invoice/userDetails.js
+++ src/components/invoice/userDetails.js
@@ -5,11 +5,11 @@ function UserDetails({user, type}){
 
   return(
     <Grid item xs={5.5} sx={{ border: 'solid 1px', borderColor: myTheme.palette.secondary.main, borderRadius: 2, p:1}}>
-      <Typography ><span style={{ color: myTheme.palette.primary.main }}>{type}: </span>{user.name}</Typography>
-      <Typography ><span style={{ color: myTheme.palette.primary.main }}>Address: </span>{user.address}</Typography>
-      <Typography ><span style={{ color: myTheme.palette.primary.main }}>Phone: </span>{user.phone}</Typography>
-      <Typography ><span style={{ color: myTheme.palette.primary.main }}>Email: </span>{user.email}</Typography>
-      <Typography ><span style={{ color: myTheme.palette.primary.main }}>Iban: </span>{user.iban}</Typography>
+      <Typography sx={{wordWrap: 'break-word'}}><span style={{ color: myTheme.palette.primary.main }}>{type}: </span>{user.name}</Typography>
+      <Typography sx={{wordWrap: 'break-word'}}><span style={{ color: myTheme.palette.primary.main }}>Address: </span>{user.address}</Typography>
+      <Typography sx={{wordWrap: 'break-word'}}><span style={{ color: myTheme.palette.primary.main }}>Phone: </span>{user.phone}</Typography>
+      <Typography sx={{wordWrap: 'break-word'}}><span style={{ color: myTheme.palette.primary.main }}>Email: </span>{user.email}</Typography>
+      <Typography sx={{wordWrap: 'break-word'}}><span style={{ color: myTheme.palette.primary.main }}>Iban: </span>{user.iban}</Typography>
 
     </Grid>
 
