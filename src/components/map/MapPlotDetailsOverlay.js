2d1814dc34dc2234fb2cbfb2ea71d880b9674d6c
MrDog210 <tusakf@gmail.com>
2024-05-29 19:08:18 +0200

    SCRUM-82 finished edit mode

--- src/components/map/MapPlotDetailsOverlay.js
+++ src/components/map/MapPlotDetailsOverlay.js
@@ -7,9 +7,11 @@ import EditIcon from '@mui/icons-material/Edit';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import { SERVER_URL } from "../../constants/http";
 import { UserContext } from "../../store/userContext";
+import EditPlot from "../plots/EditPlot";
 
 function MapPlotDetailsOverlay() {
   const [plots, setPlots] = useState(null)
+  const [editingPlot, setEditingPlot] = useState(null)
   const [selectedPlot, setSelectedPlot] = useState(null)
   const { id, title, note, plotSize, plotNumber, cadastralMunicipality, archived} = selectedPlot ?? {}
   const userCtx = useContext(UserContext)
@@ -22,14 +24,34 @@ function MapPlotDetailsOverlay() {
       },
     });
     if (data.status < 300) {
-      setPlots(plots.features.filter(p => {console.log(p.properties.id); return p.properties.id !== id}))
+      setPlots({...plots, features: plots.features.filter(p => p.properties.id !== id)})
       setSelectedPlot(null)
+      // TODO: doesnt update map
+    }
+  }
+
+  async function editPlot(plot) {
+    console.log(plot)
+    const data = await fetch(SERVER_URL + `/plots/${plot.id}`, {
+      method: 'PUT',
+      headers: {
+        "x-auth-token": userCtx.token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(plot)
+    });
+    if (data.status < 300) {
+      const json = await data.json();
+      setPlots({...plots, features: plots.features.map(p => p.properties.id === plot.id ? {...p, properties: json.plot} : p)})
+      setEditingPlot(null)
+      // TODO: doesnt update map
     }
   }
 
   return (
     <>
       <DbGeoJson plots={plots} setPlots={setPlots} onFeaturePressed={(feature, plot) => {setSelectedPlot(plot)}} />
+      {editingPlot && <EditPlot plot={editingPlot} setPlot={setEditingPlot} onConfirm={() => {editPlot(editingPlot)}} onCancel={() => {setEditingPlot(null)}} />}
       { selectedPlot &&
         <MapOverlay style={{ top: 10, right: 10, padding: 2 }}>
           <IconButton onClick={(e) => {e.stopPropagation(); setSelectedPlot(null)}}><CloseRoundedIcon fontSize="large" /></IconButton>
@@ -39,7 +61,7 @@ function MapPlotDetailsOverlay() {
           <Typography>ID: {plotNumber} {cadastralMunicipality}</Typography>
           <Typography>Is archived: {String(archived)}</Typography>
           <Box display="flex" flex={1} justifyContent="center">
-            <IconButton color="primary"><EditIcon/></IconButton>
+            <IconButton color="primary" onClick={() => {setEditingPlot(selectedPlot)}}><EditIcon/></IconButton>
             <IconButton color="secondary" onClick={deletePlot} ><DeleteIcon/></IconButton>
           </Box>
         </MapOverlay>
