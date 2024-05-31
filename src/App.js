049b57db22431a2f575c9b773954a6e8a628ebdf
mzHap <mojcazamuda12@gmail.com>
2024-05-31 07:42:25 +0200

    date localisation

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import LoginPage from "./pages/LoginPage";
 import { UserContext } from "./store/userContext";
 import Routes from "./routing";
 import SideMenu from "./components/SideMenu";
+import dayjs from "dayjs";
+require('dayjs/locale/sl');
 
 function App() {
   const userCtx = useContext(UserContext)
+  dayjs.locale("sl")
 
   if(!userCtx.token)
     return <LoginPage />
