c26471a337053b1459ed51d695aadca9a4d54f59
mzHap <mojcazamuda12@gmail.com>
2024-05-31 18:00:36 +0200

    SCRUM-105 map translation

--- src/components/map/MapEditingToggle.js
+++ src/components/map/MapEditingToggle.js
@@ -4,7 +4,7 @@ import { FormControlLabel, Switch } from "@mui/material";
 function MapEditingToggle({ checked = false, setChecked = () => {}}) {
   return (
     <MapOverlay style={{ top: 100, left: 10, padding: 1, paddingLeft: 2 }}>
-      <FormControlLabel control={<Switch checked={checked} onChange={(e, checked) => {setChecked(checked)}} />} label="Editing mode" />
+      <FormControlLabel control={<Switch checked={checked} onChange={(e, checked) => {setChecked(checked)}} />} label="Način urejanja" />
     </MapOverlay>
   )
 }
